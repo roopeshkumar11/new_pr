d7a3335aff257a9de22bb6fcad16eeb48cb31414
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2025-05-22 23:06:32 +0530

    start

---
+++ Cilent/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <>
+  <h1 className=" text-3xl font-bold underline bg-cyan-500 text-white">
+    Hello world!
+  </h1>
+</>
+
+    </>
+  )
+}
+
+export default App

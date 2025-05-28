96d9548e672169349782b50f3391e1ef2b519ce8
roopeshkumar11 <roopeshkumarbxr2017@gmail.com>
2025-05-28 23:26:18 +0530

    update

--- Cilent/src/main.jsx
+++ Cilent/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 
-import App from './App.jsx'
-import { AuthProvider } from './ContextApi/Authcontext.jsx'
+
+import App from './App'
+import './index.css'
+import { AuthProvider } from './ContextApi/Authcontext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
